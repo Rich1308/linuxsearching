a52deebc5f7d0867166a562b1a31966d43881e0c
Rich1308 <112819312+Rich1308@users.noreply.github.com>
2023-10-15 11:18:18 -0500

    Scripting to searc a file in a specific directory

---
+++ serchfile.sh
@@ -0,0 +1,7 @@
+#! /bin/bash                                                                    
+                                                                                
+if test -f $1; then                                                             
+        echo "The file exist"                                                   
+else                                                                            
+        echo "The file doesnt exist"                                            
+fi  
